409b083c4186c2b21da07833edac1b92ac1d84ff
AsMrProg16 <asmrprog16@gmail.com>
2024-05-29 21:42:47 +0500

    add exam

---
+++ 8 kyu start/Reversed Strings/reversedstring.js
@@ -0,0 +1,14 @@
+//'world'  =>  'dlrow'
+//'word'   =>  'drow'
+
+//berilgan string malumotni teskari qilish
+
+let str = 'asmrcoder'
+
+function reversedstring(str){
+  return str.split('').reverse().join('')
+}
+
+// split('') => string malumotni yoyadi
+// reverse () => string malumotni teskari qiladi
+// join('') o'sha split qilingan malumotni yig'adi
