c1076bf31a9da6d9216733cc951ab712f5c83a2d
AsMrProg16 <asmrprog16@gmail.com>
2024-05-29 22:21:32 +0500

    add

---
+++ 8 kyu start/Math Floor/math floor.js
@@ -0,0 +1,8 @@
+function litres(time) {
+  return Math.floor(time / 2);
+}
+
+//Math floor bu sonni yahlitlab beradi
+//time = 3 ----> litres = 1
+//vaqt = 3 ----> litr = 1
+//time = 11.8--> litres = 5
